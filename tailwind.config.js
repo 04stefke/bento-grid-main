07214bc1f216250851ba7eab8a486d394faf7785
04stefke <jovanovics399@yahoo.com>
2024-11-19 14:12:34 +0000

    added custom styles to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
 	content: ["./src/**/*.{html,js}", "./public/index.html", "./*.html"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				purple: {
+					100: "hsl(254, 88%, 90%)",
+					500: "hsl(256, 67%, 59%)",
+				},
+				yellow: {
+					100: "hsl(31, 66%, 93%)",
+					500: "hsl(39, 100%, 71%)",
+				},
+				white: "hsl(0, 0%, 100%)",
+				black: "hsl(0, 0%, 7%)",
+			},
+			fontSize: {
+				paragraph: "18px",
+			},
+			fontFamily: {
+				sans: ["DM Sans", "sans-serif"], // DM Sans as your default sans-serif
+			},
+			fontWeight: {
+				normal: 400,
+				medium: 500,
+			},
+		},
 	},
 	plugins: [],
 };
